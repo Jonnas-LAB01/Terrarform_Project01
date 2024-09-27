33d0e9671d68ce00f050f3ff31dcb5d169aa70c7
Jonnas <jonathan.portillo84@gmail.com>
2024-09-26 23:26:19 -0600

    Webserver working

--- main.tf
+++ main.tf
@@ -15,11 +15,45 @@ provider "aws" {
 
 #Resource Definition
 resource "aws_instance" "example" {
-    ami =           "ami-0cf10cdf9fcd62d37"
+    ami =           "ami-0e54eba7c51c234f6"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo yum install -y httpd
+                sudo systemctl start httpd
+                echo "Hello World" | sudo tee /var/www/html/index.html
+                EOF
+
+    user_data_replace_on_change = true
 
     tags = {
         Name = "Terraform-Example"
     }
 
 }
+
+resource "aws_security_group" "instance" {
+
+  name = "terraform-example-instance"
+
+ ingress {
+    from_port = 80  
+    to_port = 80    
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["18.206.107.24/29"]  # EC2 Instance Connect IP range
+  }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
